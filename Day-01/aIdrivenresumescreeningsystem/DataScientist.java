aa3092f0764e2b119536f5383862d1a91cf47c97
Sandeep123vi <sandeepvishwakarna2822@gmail.com>
2025-02-11 13:04:28 +0530

    [Sandeep] Add. this class extends Jobrole class

---
+++ Day-01/aIdrivenresumescreeningsystem/DataScientist.java
@@ -0,0 +1,7 @@
+package aIdrivenresumescreeningsystem;
+
+class DataScientist extends JobRole {
+    public DataScientist(String candidateName, int experienceYears) {
+        super(candidateName, experienceYears);
+    }
+}
