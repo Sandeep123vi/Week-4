e455b79f72a98a0f0e315fe4210d356f5ee74f11
Sandeep123vi <sandeepvishwakarna2822@gmail.com>
2025-02-11 12:19:08 +0530

    [Sandeep] this extends Coursetype class

---
+++ Day-01/universitymanagement/ResearchCourse.java
@@ -0,0 +1,7 @@
+package universitymanagement;
+
+class ResearchCourse extends CourseType {
+    public ResearchCourse(String courseName, String instructor) {
+        super(courseName, instructor);
+    }
+}
